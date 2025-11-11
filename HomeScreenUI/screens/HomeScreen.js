ccea56929856f9b996e308a521fee815ec2eadf5
Rajkiumar <sumathiselvam0007@gmail.com>
2025-11-11 12:20:29 +0530

    Building Home Screen 2

--- HomeScreenUI/screens/HomeScreen.js
+++ HomeScreenUI/screens/HomeScreen.js
@@ -4,14 +4,45 @@ import Header from '../components/Header';
 import ProductCard from '../components/ProductCard';
 
 export default function HomeScreen() {
+  const products = [
+    {
+      id: 1,
+      title: 'Handmade Basket',
+      price: 499,
+      image: 'https://m.media-amazon.com/images/I/61fUwilhjJL._AC_UF350,350_QL80_.jpg',
+    },
+    {
+      id: 2,
+      title: 'Clay Pot',
+      price: 299,
+      image: 'https://img.indiahandmade.com/catalog/product/cache/dee0bc41489afb86ae85561eae1bc64e/s/m/smallc_2.png',
+    },
+    {
+      id: 3,
+      title: 'Jute Bag',
+      price: 799,
+      image: 'https://uneako.com/wp-content/uploads/2020/03/17.webp',
+    },
+    {
+      id: 4,
+      title: 'Wooden Toy',
+      price: 699,
+      image: 'https://cdn.shopify.com/s/files/1/0067/8179/6450/files/educational_toys_large.png?v=1556594037',
+    },
+  ];
+
   return (
     <View style={styles.container}>
       <Header />
       <ScrollView contentContainerStyle={styles.productsContainer}>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {products.map((item) => (
+          <ProductCard
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
       </ScrollView>
     </View>
   );
@@ -20,12 +51,12 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f2f2f2',
+    backgroundColor: '#f9fafc',
   },
   productsContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'center',
-    paddingBottom: 20,
+    paddingVertical: 20,
   },
 });
